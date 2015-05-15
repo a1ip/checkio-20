e6cc665eabf0a1ba6aa63cdb0481bc75a14248e5
no0b1kk <noJIoSka@gmail.com>
2015-05-15 09:43:53 +0300

    solve non-unique elements

--- home/non-unique_elements.py
+++ home/non-unique_elements.py
@@ -2,7 +2,7 @@ def checkio(data):
     return [x for x in data if data.count(x) > 1]
 
 if __name__ == "__main__":
-
+#These "asserts" using only for self-checking and not necessary for auto-testing
     assert isinstance(checkio([1]), list), "The result must be a list"
     assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
     assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
