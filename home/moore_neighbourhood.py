4d6aebd4b75acd77c24f8ba359bd613992fca9b1
no0b1kk <noJIoSka@gmail.com>
2015-06-19 14:03:47 +0300

    decision moore neighbourhood

---
+++ home/moore_neighbourhood.py
@@ -0,0 +1,26 @@
+def count_neighbours(grid, row, col):
+    f = 0
+    for i in range(row-1, row+2):
+        for j in range(col-1, col+2):
+            if i >=0 and i <= len(grid)-1 and j >= 0 and j <= len(grid[0])-1:
+                f += grid[i][j]
+    f = f - grid[row][col]
+    return f
+
+if __name__ == '__main__':
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 1, 2) == 3, "1st example"
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 0, 0) == 1, "2nd example"
+    assert count_neighbours(((1, 1, 1),
+                             (1, 1, 1),
+                             (1, 1, 1),), 0, 2) == 3, "Dense corner"
+    assert count_neighbours(((0, 0, 0),
+                             (0, 1, 0),
+                             (0, 0, 0),), 1, 1) == 0, "Single"
